db7be5114763d1f0e63b50982c6569cc4ecbd379
unknown <andrei131298@yahoo.com>
2022-03-19 20:06:33 +0200

    first commit

---
+++ TestApp/Contexts/Context.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace TestApp.Contexts
+{
+    public class Context: DbContext
+    {
+        //public DbSet<City> Cities { get; set; }
+       
+        public static bool isMigration = true;
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (isMigration)
+                optionsBuilder.UseSqlServer(@"Server=.\;Database=VacayCheckDB;Trusted_Connection=true;");           
+        }
+        public Context() { }
+        public Context(DbContextOptions<Context> options) : base(options) { }
+
+    }
+
+}
---
+++ TestApp/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using TestApp.Models;
+
+namespace TestApp.Controllers
+{
+    [Route("api/home")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        public HomeController()
+        {
+        }
+
+        // GET: api/Home
+        [HttpGet]
+        public string Get()
+        {
+            return "corect";
+        }
+
+    }
+}
